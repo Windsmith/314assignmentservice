a52e0cf9ceacdc77dd3ed18b1fa72581e7077eb6
Windsmith <shriramsekar11@gmail.com>
2023-06-21 16:49:07 +0400

    feat: make assignment page dynamic

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,28 @@
 const express = require('express')
+const fs = require('fs')
 const router = express.Router()
 
 router.get('/', (req, res) => {
     res.sendFile(__dirname + '/pages/assignmentDashboard.html')
 })
 
-router.get('/assignment', (req, res) => {
+router.get('/assignment/:id', (req, res) => {
     res.sendFile(__dirname + '/pages/assignment.html')
 })
+
+router.get('/assignmentData/:id', (req, res) => {
+    let id = req.params.id
+    let assignments = JSON.parse(fs.readFileSync(__dirname + '/assignments.json'))
+    assignments = assignments.filter((element) => {
+        return element.id == parseInt(id, 10);
+    })
+
+    res.json(assignments[0])
+})
+
+router.get('/download', function (req, res) {
+    const file = __dirname + '/static/studyMaterial.docx';
+    res.download(file); // Set disposition and send it.
+});
 
 module.exports = router;
